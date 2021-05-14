b6cbaa4d509f8d669e9c493a18254b6c7bfd2f2b
Esubalew Dessalegn <scotchtour1@gmail.com>
2021-05-14 12:27:02 -0400

    we made some changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 const ourTaskManager = new taskManager();
 ourTaskManager.addTask('Cook Dinner','Take out the trash','Nick','2020-09-20','TODO');
 console.log(ourTaskManager.tasks);
+var string = document.location + '';
+let tasksHtmlList = ['Go shopping','pay rent','do laundry','get groceries'];
+tasksHtmlList = tasksHtmlList.split(",");
+
+for (let i = 0; i < tasksHtmlList.length; i++) {
+  tasksHtmlList[i] = " " + tasksHtmlList[i] + "\n";
+}
+console.log(tasksHtmlList); 
+tasksHtmlList = tasksHtmlList.join("");
+console.log(tasksHtmlList);
+document.getElementById("ovde").innerHTML = customItems;
 const taskHtml = createTaskHtml('Cook Dinner','Take out the trash','Nick','2020-09-20','TODO');
 console.log(taskHtml);
+
+
+
+
+
+
+
